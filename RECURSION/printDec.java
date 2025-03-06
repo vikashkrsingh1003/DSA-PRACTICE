ced3372ac46912837a07702a056bc8056cd30b3b
vikashkrsingh1003 <vksingh1062003@gmail.com>
2025-03-06 12:46:28 +0530

    6 march

--- RECURSION/printDec.java
+++ RECURSION/printDec.java
@@ -11,10 +11,24 @@ public class printDec {
     printNum(n - 1);
   }
 
+  public static void printInc(int m) {
+
+    if (m == 11) {
+      //System.out.println(n);
+      return;
+    }
+   
+    System.out.print("==>" + m + " ");
+    printInc(m + 1);
+  }
+
   public static void main(String args[]) {
 
-    int n = 10;
-    printNum(n);
+    // int n = 10;
+    // printNum(n);
+
+    int m =1;
+    printInc(m);
   }
 
 }
