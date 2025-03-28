2a1c6726c9472b00644c39d829af63de0fa76d69
vikashkrsingh1003 <vksingh1062003@gmail.com>
2025-03-28 15:37:32 +0530

    Backtracking

--- BackTracking/stringSubset.java
+++ BackTracking/stringSubset.java
@@ -1,10 +1,25 @@
 package BackTracking;
 
 public class stringSubset {
-    
+       
+      public static void findSubset(String str, String ans, int i){
+
+        //base case 
+        if(i == str.length()){
+            System.out.println(ans);
+             return;
+        }
+        //yes choice
+        findSubset(str, ans+str.charAt(i), i+1);
+
+        //No choice
+        findSubset(str, ans, i+1);
+      } 
+
 public static void main(String[] args) {
     
 
-    String s = " welcom , "
+    String str = "abc";
+    findSubset(str, "", 0);
 }
 }
