d83b1c71e02004cdefc90cb61e2b53925bc7d28d
vikashkrsingh1003 <vksingh1062003@gmail.com>
2025-03-29 23:46:24 +0530

    DSA

--- tnp/p1.java
+++ tnp/p1.java
@@ -3,6 +3,7 @@ package tnp;
 import java.util.*;
 public class p1 {
     
+    //ToggleProblem 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
